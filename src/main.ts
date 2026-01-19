88a7ba6914fde4968438bd91dd5398c7a0e8f579
Dmitry Kalinin <dmitriy.kalinin@medrocket.ru>
2026-01-19 09:49:33 +0300

    chore: add ag-grid-vue3 package

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { ModuleRegistry } from 'ag-grid-enterprise';
+import { AllEnterpriseModule } from 'ag-grid-enterprise';
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+ModuleRegistry.registerModules([AllEnterpriseModule]);
+
 createApp(App).mount('#app')
